b7e1bc0082e1494543e3f02c0055edf3a1d59bd9
Kangnos <rkdtlsgur66@gmail.com>
2021-01-02 22:48:43 +0900

    2021 01 02

--- src/App.js
+++ src/App.js
@@ -1,12 +1,64 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      Hello, React!!
-    </div>
-  );
+
+// 함수식
+// function App() {
+//   return (
+//     <div className="App">
+//       Hello, React!!
+//     </div>
+//   );
+// }
+
+class Subject extends Component{
+  render(){
+    return ( // 하나의 최상의 태그만 가능함
+      <header> 
+        <h1>WEB</h1>
+        world wide web
+      </header>
+    );
+  }
+}
+
+
+class Nav extends Component{
+  render(){
+    return(
+      <nav>
+        <ul>
+            <li> <a href="1.html">HTML</a></li>
+            <li> <a href="2.html">CSS</a> </li>
+            <li> <a href="3.html">JavaScript</a> </li>
+        </ul>
+      </nav>
+    );
+  }
+}
+
+class Article extends Component{
+  render(){
+    return(
+      <article>
+        <h2>HTML</h2>
+        HTML is HyperText Markup Language.
+      </article>
+    )
+  }
+}
+
+class App extends Component { // 컴포넌트를 만드는 코드
+  render(){
+    return (
+      <div className="App">
+        <Subject></Subject>
+        <Nav></Nav>
+        <Article></Article>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from './App.js'; // './App'이라고 해도됨 
 
 test('renders learn react link', () => {
   render(<App />);
