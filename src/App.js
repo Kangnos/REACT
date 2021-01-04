f25c99469a4498546f335147d8ff15d89e983ba7
Kangnos <rkdtlsgur66@gmail.com>
2021-01-04 23:25:56 +0900

    2021 01 04

--- src/App.js
+++ src/App.js
@@ -20,7 +20,9 @@ class App extends Component { // 컴포넌트를 만드는 코드
   constructor(props) { // render()보다 먼저 실행되면서 props를 초기화
     super(props);
     this.state = {
+      mode:'welcome',
       subject: { title: 'WEB', sub: 'World Wide Web!' },
+      welcome:{title:'Welcome', desc:'Hello React'},
       contents:[
         {id:1, title:'HTML', desc:'HTML is HyperText Markup Language'},
         {id:2, title:'CSS', desc:'CSS is for design'},
@@ -29,13 +31,34 @@ class App extends Component { // 컴포넌트를 만드는 코드
     }
   }
   render() {
+    var _title, _desc = null;
+    if (this.state.mode === "welcome" || this.state.mode === "Welcome") {
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    }
+    else if (this.state.mode == "read") {
+      _title = this.state.contents[0].title;
+      _desc = this.state.contents[0].desc;
+    }
     return (
       <div className="App">
         {/* state로부터 옴 */}
-        <Subject title={this.state.subject.title} sub={this.state.subject.sub}></Subject>
-        <Subject title="React" sub="For UI"></Subject>
+        {/* <Subject 
+          title={this.state.subject.title} 
+          sub={this.state.subject.sub}>
+        </Subject> */}
+        <header>
+          <h1><a href="/"onClick={function(e) {
+            console.log("e")
+            e.preventDefault();
+            this.setState({
+              mode:'welcome'
+            })
+          }.bind(this)}>{this.state.subject.title}</a></h1>
+          {this.state.subject.sub}
+        </header>
         <Nav data={this.state.contents}></Nav>
-        <Article title="HTML" contents="HTML is HyperText Markup Language."></Article>
+        <Article title={_title} contents={_desc}></Article>
       </div>
     );
   }
--- src/components/Subject.js
+++ src/components/Subject.js
@@ -4,7 +4,7 @@ class Subject extends Component {
     render() {
         return ( // 하나의 최상의 태그만 가능함
             <header>
-                <h1>{this.props.title}</h1>
+                <h1><a href="/">{this.props.title}</a></h1>
                 {this.props.sub}
             </header>
         );
